2c73b5c7929d9af06fef62d733c6ce8ba7b91e20
shida-zheng-ICD2O1-01 <shida.zheng@student.tdsb.on.ca>
2024-11-07 15:05:12 +0000

    a

--- PP1_7.py
+++ PP1_7.py
@@ -1,25 +1,37 @@
 
 
-q1():
+def q1():
   #Write code here
-
-q2():
+  trueOrFalse = True
+  print(trueOrFalse)
+def q2():
   #Write code here
-
-q3():
+  num = int(input("Input an integer: "))
+  bool1 = num > 5
+  print(bool1)
+def q3():
   #Write code here
-
-q4():
+  letter = input("Input the letter a:")
+  bool2 = (letter == "a")
+  print(bool2)
+def q4():
   #Write code here
-
-q5():
+  word = input("Input a word earlier in the dictionary than google: ")
+  bool3 = (word < "google")
+  print(bool3)
+def q5():
   #Write code here
+  num1 = int(input("Input an integer: "))
+  num2 = int(input("Input another integer: "))
+  num3 = (num1 * num2)
+  bool4 = num3 > 40
+  print(f"Your numbers multiplied together are greater than 40: {bool4}")
 
 #Do edit the code below
 #Comment the lines below when running your tests
 
-q1()
-q2()
-q3()
-q4()
-q5()
+#q1()
+#q2()
+#q3()
+#q4()
+#q5()
